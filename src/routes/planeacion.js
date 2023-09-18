1e920325c0065886411b330a62786dc0a84bdf28
Enrique <enrique_alvarado95@hotmail.com>
2023-09-18 13:13:46 -0600

    added support for .xls (older excel) files

--- src/routes/planeacion.js
+++ src/routes/planeacion.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import csv from "csv-parser";
 import multer from "multer";
 import streamifier from "streamifier";
-import xlsx from "xlsx"; // Importa la biblioteca xlsx
+import xlsx from "xlsx";
 
 const planeacionRouter = Router();
 const storage = multer.memoryStorage();
@@ -30,8 +30,11 @@ planeacionRouter.post("/archivo", upload.single("file"), async (req, res) => {
       .on("end", () => {
         res.json(data);
       });
-  } else if (req.file.originalname.endsWith(".xlsx")) {
-    // Si es un archivo XLSX
+  } else if (
+    req.file.originalname.endsWith(".xlsx") ||
+    req.file.originalname.endsWith(".xls")
+  ) {
+    // Si es un archivo XLSX o XLS
     try {
       const workbook = xlsx.read(fileBuffer, { type: "buffer" });
       const sheetName = workbook.SheetNames[0]; // Suponemos que solo hay una hoja
@@ -40,7 +43,7 @@ planeacionRouter.post("/archivo", upload.single("file"), async (req, res) => {
 
       res.json(data);
     } catch (error) {
-      res.status(500).json({ message: "Error al parsear el archivo XLSX" });
+      res.status(500).json({ message: "Error al parsear el archivo" });
     }
   } else {
     res.status(400).json({ message: "Tipo de archivo no compatible" });
